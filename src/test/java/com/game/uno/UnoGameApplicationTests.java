7a1e92875f3aba742a59cdee77798b133b7dd67c
Erika Viveros <viverozerika@gmail.com>
2025-12-16 19:48:01 -0600

    Primer commit

---
+++ src/test/java/com/game/uno/UnoGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.uno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
